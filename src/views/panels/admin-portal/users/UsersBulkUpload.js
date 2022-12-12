d0b0e32c433cd4f23ec3497f3a5c37c40de9c873
Veera Raghavan <smiore97@yahoo.com>
2022-12-12 15:37:05 +0530

    Directory Changes

---
+++ src/views/panels/admin-portal/users/UsersBulkUpload.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Dashboard from '../../../common-ui-components/Dashboard';
+
+const AdminDashboard = () => {
+  return (
+    <React.Fragment>
+      <Dashboard />
+    </React.Fragment>
+  );
+};
+
+export default AdminDashboard;
