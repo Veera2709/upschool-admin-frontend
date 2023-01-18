c7ade572eafa8ef6ca9f053cb59c21e7d5fcff1d
sudharshana.reddy@rhibhus.com <sudharshana.reddy@rhibhus.com>
2023-01-18 16:01:53 +0530

    validation in section creation

--- src/views/panels/admin-portal/school/CreateSection.js
+++ src/views/panels/admin-portal/school/CreateSection.js
@@ -23,7 +23,8 @@ const CreateSection = ({ setOpenAddSection, id }) => {
     const colourOptions = [];
     let history = useHistory();
     const [options, setOptions] = useState([]);
-    const [classId, setClass] = useState([]);
+    const [classId, setClass] = useState();
+    const [classIdErr, setClassIdErr] = useState(false);
 
     const MySwal = withReactContent(Swal);
 
@@ -60,9 +61,7 @@ const CreateSection = ({ setOpenAddSection, id }) => {
                         console.log();
                         setOpenAddSection(false)
                         sweetAlertHandler({ title: 'Sorry', type: 'error', text: 'Section Name Already Exists!' })
-
                     } else if (error.response.data === 'Invalid Token') {
-
                         sessionStorage.clear();
                         localStorage.clear();
                         history.push('/auth/signin-1');
@@ -70,118 +69,126 @@ const CreateSection = ({ setOpenAddSection, id }) => {
                     } else {
                         console.log("err", error);
                     }
-                
-            } else if (error.request) {
-                // The request was made but no response was received
-                console.log(error.request);
-            } else {
-            // Something happened in setting up the request that triggered an Error
-            console.log('Error', error.message);
-        }
-    });
-}
 
-const fetchAllClassData = async () => {
-    const allClassData = await fetchClassBasedOnSchool(id);
-    console.log("allClassData", allClassData);
-    if (allClassData.Error) {
-        console.log("allClassData.Error", allClassData.Error);
-        if (allClassData.Error.response.data == 'Invalid Token') {
-            sessionStorage.clear();
-            localStorage.clear();
-            history.push('/auth/signin-1');
-            window.location.reload();
-        }
-    } else {
-        let resultData = allClassData.Items;
-        resultData.forEach((item, index) => {
-            colourOptions.push({ value: item.client_class_id, label: item.client_class_name })
-        })
-        setOptions(colourOptions)
-
-    }
-}
-
-useEffect(() => {
-    fetchAllClassData()
-}, [])
-
-const classOption = (e) => {
-    console.log("postPreOption", e);
-    setClass(e.value);
-};
-return (
-    <React.Fragment>
-        <Formik
-            initialValues={{
-                section_name: '',
-                class_id: '',
-            }}
-
-            validationSchema={Yup.object().shape({
-                section_name: Yup.string()
-                    .trim()
-                    .required(Constants.AddSection.SectionTitleRequired),
-            })}
-            // validationSchema
-            onSubmit={(values, { setErrors, setStatus, setSubmitting }) => {
-                setSubmitting(true);
-                // setOpenAddSection(false)
-                const formData = {
-                    section_name: values.section_name,
-                    client_class_id: classId,
-                    school_id: id
+                } else if (error.request) {
+                    // The request was made but no response was received
+                    console.log(error.request);
+                } else {
+                    // Something happened in setting up the request that triggered an Error
+                    console.log('Error', error.message);
                 }
-                console.log('formData: ', formData)
-                Addsection(formData)
-            }}
-        >
-            {({ errors, handleBlur, handleChange, handleSubmit, touched, values }) => (
-                <Form onSubmit={handleSubmit} >
-                    <Col sm={6}>
-                        <Form.Group>
-                            <Form.Label className="floating-label" htmlFor="section_name"><small className="text-danger">* </small>Section Name</Form.Label>
-                            <Form.Control
-                                className="form-control"
-                                name="section_name"
-                                onBlur={handleBlur}
-                                onChange={handleChange}
-                                type="text"
-                                value={values.section_name}
-                            />
-                            {touched.section_name && errors.section_name && <small className="text-danger form-text">{errors.section_name}</small>}
-                        </Form.Group>
-                    </Col>
+            });
+    }
 
-                    <Col sm={6}>
+    const fetchAllClassData = async () => {
+        const allClassData = await fetchClassBasedOnSchool(id);
+        console.log("allClassData", allClassData);
+        if (allClassData.Error) {
+            console.log("allClassData.Error", allClassData.Error);
+            if (allClassData.Error.response.data == 'Invalid Token') {
+                sessionStorage.clear();
+                localStorage.clear();
+                history.push('/auth/signin-1');
+                window.location.reload();
+            }
+        } else {
+            let resultData = allClassData.Items;
+            resultData.forEach((item, index) => {
+                colourOptions.push({ value: item.client_class_id, label: item.client_class_name })
+            })
+            setOptions(colourOptions)
 
-                        <div className="form-group fill">
-                            <label className="floating-label" >
-                                <small className="text-danger">* </small>
-                                Class
-                            </label>
-                            <Select
-                                className="basic-single"
-                                classNamePrefix="select"
-                                name="color"
-                                options={options}
-                                onChange={(e) => { classOption(e) }}
-                            />
-                        </div>
-                    </Col>
-                    <div className="row d-flex justify-content-end">
-                        <div className="form-group fill">
-                            <div className="center col-sm-12">
-                                <button color="success" type="submit" className="btn-block btn btn-success btn-large">Submit</button>
+        }
+    }
+
+    useEffect(() => {
+        fetchAllClassData()
+    }, [])
+
+    const classOption = (e) => {
+        console.log("postPreOption", e);
+        setClass(e.value);
+    };
+    return (
+        <React.Fragment>
+            <Formik
+                initialValues={{
+                    section_name: '',
+                    class_id: '',
+                }}
+
+                validationSchema={Yup.object().shape({
+                    section_name: Yup.string()
+                        .trim()
+                        .required(Constants.AddSection.SectionTitleRequired),
+                })}
+                // validationSchema
+                onSubmit={(values, { setErrors, setStatus, setSubmitting }) => {
+                    // setSubmitting(true);
+                    // setOpenAddSection(false)
+                    if (classId === undefined || classId === '') {
+                        setClassIdErr(true)
+                    }else{
+                        const formData = {
+                            section_name: values.section_name,
+                            client_class_id: classId,
+                            school_id: id
+                        }
+                        console.log('formData: ', formData)
+                        Addsection(formData)
+                    } 
+                }}
+            >
+                {({ errors, handleBlur, handleChange, handleSubmit, touched, values }) => (
+                    <Form onSubmit={handleSubmit} >
+                        <Col sm={6}>
+                            <Form.Group>
+                                <Form.Label className="floating-label" htmlFor="section_name"><small className="text-danger">* </small>Section Name</Form.Label>
+                                <Form.Control
+                                    className="form-control"
+                                    name="section_name"
+                                    onBlur={handleBlur}
+                                    onChange={handleChange}
+                                    type="text"
+                                    value={values.section_name}
+                                />
+                                {touched.section_name && errors.section_name && <small className="text-danger form-text">{errors.section_name}</small>}
+                            </Form.Group>
+                        </Col>
+
+                        <Col sm={6}>
+
+                            <div className="form-group fill">
+                                <label className="floating-label" >
+                                    <small className="text-danger">* </small>
+                                    Class
+                                </label>
+                                <Select
+                                    className="basic-single"
+                                    classNamePrefix="select"
+                                    name="color"
+                                    options={options}
+                                    onChange={(e) => {
+                                        classOption(e);
+                                        setClassIdErr(false)
+                                    }}
+                                />
+                            </div>
+                            {classIdErr && (<small className="text-danger form-text">Select The Class!</small>)}
+                        </Col>
+                        <div className="row d-flex justify-content-end">
+                            <div className="form-group fill">
+                                <div className="center col-sm-12">
+                                    <button color="success" type="submit" className="btn-block btn btn-success btn-large">Submit</button>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                </Form>
-            )}
-        </Formik>
-    </React.Fragment>
-)
+                    </Form>
+                )}
+            </Formik>
+        </React.Fragment>
+    )
 }
 
 export default CreateSection
